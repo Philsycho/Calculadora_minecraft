d9282ce565f2f14436594497198928e405a7bb2d
Juarez P Pedroso <55813177+Philsycho@users.noreply.github.com>
2025-05-01 20:54:32 +0000

    Organização Pasta e códigos

---
+++ src/scripts/script.js
@@ -0,0 +1,24 @@
+/*
+ * 
+ * Função para incrementar a quantidade no input do html.
+ * 
+ */
+
+function incrementar(botao) {
+    const container = botao.closest(".select-itens-craft");
+    const input = container.querySelector("input[type='number']");
+    let atual = parseInt(input.value) || 0;
+    if (atual < parseInt(input.max)) {
+        input.value = atual + 1;
+    }
+}
+
+/**
+ * 
+ * Função para exibir as opções de receitas no html
+ * 
+ */
+
+function exibeOpcoes() {
+    
+}
